50c79a80d09a39aad911edcd3d65a6ade45d719e
midaso190 <43401348+midaso190@users.noreply.github.com>
2020-05-12 12:23:07 +0200

    De laatste twee opdrachten

---
+++ Hoofdstuk2Opdr2.rb
@@ -0,0 +1,13 @@
+getal = gets.to_i
+
+if getal % 2 == 0
+  puts "#{getal} is geen priemgetal."
+elsif getal % 3 == 0
+  puts "#{getal} is geen priemgetal."
+elsif getal % 5 == 0
+  puts "#{getal} is geen priemgetal."
+elsif getal % 7 == 0
+  puts "#{getal} is geen priemgetal."
+else
+  puts "#{getal} is een priemgetal."
+end
---
+++ Hoofdstuk2Opdr3.rb
@@ -0,0 +1,7 @@
+counter = 0
+while counter < 100
+  counter = counter + 1
+  if counter % 3 == 0 && counter % buzz == 0
+    puts "#{counter}"
+end
+end
