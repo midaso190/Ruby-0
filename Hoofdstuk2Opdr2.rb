06ba2b10a7d92ebf7830f459a2d14d3d01e01a4f
midaso190 <midassteeman@gmail.com>
2020-05-18 12:39:59 +0200

    Update Hoofdstuk2Opdr2.rb

--- Hoofdstuk2Opdr2.rb
+++ Hoofdstuk2Opdr2.rb
@@ -1,6 +1,7 @@
 getal = gets.to_i
-
-if getal % 2 == 0 || getal % 3 || getal % 5 || getal % 7
+if getal == 2 || getal == 3 || getal == 5 || getal == 7
+  puts "#{getal} is een priemgetal."
+elsif getal % 2 == 0 || getal % 3 || getal % 5 || getal % 7
   puts "#{getal} is geen priemgetal."
 else
   puts "#{getal} is een priemgetal."
