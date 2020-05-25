295ee5a5db7193bfe9d265cabf29efda35bdbb67
midaso190 <midassteeman@gmail.com>
2020-05-25 11:47:16 +0200

    Woepkroep
    AAAA

--- Hoofdstuk2Opdr2.rb
+++ Hoofdstuk2Opdr2.rb
@@ -1,7 +1,7 @@
 getal = gets.to_i
 if getal == 2 || getal == 3 || getal == 5 || getal == 7
   puts "#{getal} is een priemgetal."
-elsif getal % 2 == 0 || getal % 3 || getal % 5 || getal % 7
+elsif getal % 2 == 0 || getal % 3 == 0 || getal % 5 == 0 || getal % 7 == 0
   puts "#{getal} is geen priemgetal."
 else
   puts "#{getal} is een priemgetal."
